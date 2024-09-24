eeb03cced166545c4bb93951d6bbe8b1faeb920e
alan890729 <alan890729@gmail.com>
2024-09-24 23:53:43 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,6 +20,23 @@ const restaurantController = {
         return res.render('restaurants', { restaurants: data })
       })
       .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category
+        }
+      ],
+      raw: true,
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant didn\'t exist!')
+
+        return res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
